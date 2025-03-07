c725e59e84b365676ece300942e8bfe34252c5b0
謝侑諭 <franky@xieyouyudeMacBook-Pro.local>
2025-03-07 15:57:18 +0800

    Complete newReact-stars.js

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState}from 'react'
 import ReactDOM from 'react-dom/client';
 import Stars from '../src/react-stars'
 import NewStars from '../src/newReact-stars'
@@ -30,27 +30,45 @@ const thirdExample = {
   }
 }
 
+function App() {
+  const [value, setValue] = useState(5.5);
+  
+  const fourthExample = {
+    size: 40,
+    count: 10,
+    value: value,
+    onChange: newValue => {
+      console.log(`Example 4: new value is ${newValue}`);
+      setValue(newValue);
+    }
+  }
+  
+  return (
+    <div>
+      <h1>react-stars examples</h1>
+      <p><i>Star rating component for your React projects</i></p>
+      Custom size, preset value, not editable
+      <Stars {...firstExample} />
+      Custom character, custom colors, 10 stars
+      <Stars {...secondExample} />
+      Editable, preset value, half stars off
+      <Stars {...thirdExample} />
+
+      <br/>
+      --------Use new React version complete----------
+      <NewStars {...fourthExample}/> 
+      <div>Current value: {value} / {fourthExample.count}</div>
+      <br/>
+
+      <a href="https://github.com/n49/react-stars">Github</a>&nbsp;|&nbsp;
+      <a href="https://www.npmjs.com/package/react-stars">NPM package</a>
+    </div>
+  );
+
+
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <div>
-    <h1>react-stars examples</h1>
-    <p><i>Star rating component for your React projects</i></p>
-    Custom size, preset value, not editable
-    <Stars {...firstExample} />
-    Custom character, custom colors, 10 stars
-    <Stars {...secondExample} />
-    Editable, preset value, half stars off
-    <Stars {...thirdExample} />
+root.render(<App />);
 
-
-    <br/>
-    --------Use new React version complete----------
-    <NewStars/> 
-    <br/>
-
-    <a href="https://github.com/n49/react-stars">Github</a>&nbsp;|&nbsp;
-    <a href="https://www.npmjs.com/package/react-stars">NPM package</a>
-  </div>,
-  document.getElementById('root')
-)
